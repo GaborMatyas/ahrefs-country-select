45166b2cf9f36c8de61e27e2d2d2ca9ec8c95042
Melkor <matyasgabor.1987@gmail.com>
2023-10-23 14:28:09 +0200

    chore: update config files and dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,18 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.bs.mjs"],
   theme: {
+    colors: {
+      lightTextPrimary: "#333333",
+      lightBorderControlAlpha: "rgba(0, 0, 0, 0.20)",
+      lightBackgroundSelected: "#FFDBB3",
+    },
     extend: {
       height: {
-        'screen-dvh': [
-          '100vh',
-          '100dvh'
-        ],
+        "screen-dvh": ["100vh", "100dvh"],
       },
       width: {
-        'screen-dvw': [
-          '100vw',
-          '100dvw'
-        ],
+        "screen-dvw": ["100vw", "100dvw"],
       },
     },
   },
