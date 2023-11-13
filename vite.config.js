be586a9a3f7ccdccafa59892cc786e08e867f07a
Melkor <matyasgabor.1987@gmail.com>
2023-11-13 18:55:45 +0100

    build: exclude decco

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [
     react({
       include: ["**/*.bs.mjs"],
+      exclude: ["decco/*/*.bs.mjs"],
     }),
   ],
+  build: {
+    rollupOptions: {
+      external: ["decco/src/Decco.bs.mjs"],
+    },
+  },
 });
